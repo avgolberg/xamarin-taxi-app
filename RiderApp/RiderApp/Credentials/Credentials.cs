616c7890b28150397a240a3c2fa8c6dea0b9be80
Alyona Berezorutskaya <av.golberg@gmail.com>
2022-09-02 09:37:08 +0300

    Revert "Add credential files to .gitignore"
    This reverts commit f8d8dce248b4db0313c13689b2c6be68801f42fe.

---
+++ RiderApp/RiderApp/Credentials/Credentials.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiderApp.Credentials
+{
+    public class Credentials
+    {
+        public static string API_KEY = "Your Google Maps SDK API Key";
+    }
+}
