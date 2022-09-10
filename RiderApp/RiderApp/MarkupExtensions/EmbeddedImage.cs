eb6e63194e151ba9731a894cb1fa4a52ad1b4a51
Alyona Berezorutskaya <av.golberg@gmail.com>
2022-09-10 14:56:41 +0300

    Add rider main page

---
+++ RiderApp/RiderApp/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RiderApp.MarkupExtensions
+{
+    [ContentProperty("ResourseId")]
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourseId { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (String.IsNullOrWhiteSpace(ResourseId))
+                return null;
+            return ImageSource.FromResource(ResourseId);
+        }
+    }
+}
