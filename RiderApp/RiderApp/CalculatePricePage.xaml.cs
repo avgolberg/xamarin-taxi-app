5dce1497ef3ff7e4c2a5bdee119d34203b169f87
Alyona Berezorutskaya <av.golberg@gmail.com>
2022-12-08 19:32:50 +0200

    Add CalculatePricePage without logic

---
+++ RiderApp/RiderApp/CalculatePricePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RiderApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculatePricePage : ContentPage
+    {
+        public CalculatePricePage()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseTapped(object sender, EventArgs e)
+        {
+            FlyoutPage menu = (FlyoutPage)Application.Current.MainPage;
+            menu.IsPresented = true;
+            menu.Detail = App.mainPage;
+        }
+    }
+}
